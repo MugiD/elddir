626673e6e5a5af46cf1c98660bfc6e7335dd6179
MugiD <bekslambek22@gmail.com>
2024-08-06 22:47:12 +0500

    ux: deletion confirmation

--- src/components/category/deleteAuthors.tsx
+++ src/components/category/deleteAuthors.tsx
@@ -1,14 +1,43 @@
 import { deleteInfluencer } from "@/actions/action";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
 
-export default function DeleteAuthors({id, route}: any) {
+export default function DeleteAuthors({ id, route }: any) {
   const deleteInfluencerWithId = deleteInfluencer.bind(null, id, route);
 
   return (
-    <form action={deleteInfluencerWithId} className="inline">
-      <Button type="submit" variant="link" className="p-0">
-        Delete
-      </Button>
-    </form>
+    <main>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button type="submit" variant="link" className="p-0">
+            Delete
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Delete category</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete this category?
+            </DialogDescription>
+          </DialogHeader>
+          <form action={deleteInfluencerWithId}>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="submit">Delete</Button>
+              </DialogClose>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </main>
   );
 }
